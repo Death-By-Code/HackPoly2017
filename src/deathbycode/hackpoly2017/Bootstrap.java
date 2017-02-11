57fa85a90301da37ccd7cc58e9e2f03da45544f0
Joshua Rodriguez <ewized@gmail.com>
2017-02-11 16:51:31 -0600

    Create bootstrap

---
+++ src/deathbycode/hackpoly2017/Bootstrap.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2017 Death-By-Code
+ */
+package deathbycode.hackpoly2017;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

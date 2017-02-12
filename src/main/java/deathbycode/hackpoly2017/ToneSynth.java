f8dab73929831a4e01a4080d157c1f051ec7e675
Joseph <Joseph>
2017-02-11 19:19:35 -0800

    revised the play member function

---
+++ src/main/java/deathbycode/hackpoly2017/ToneSynth.java
@@ -0,0 +1,32 @@
+import javax.sound.midi.Instrument;
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+
+/**
+ * Created by Acer Customer on 2/11/2017.
+ */
+public class ToneSynth {
+    Synthesizer syn;
+    MidiChannel[] midichannel;
+    Instrument[] instruments;
+
+    public ToneSynth() {
+        try {
+            syn = MidiSystem.getSynthesizer();
+            syn.open();
+            midichannel = syn.getChannels();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void play(int... notes) {
+        for(int i = 0; i < notes.length; i++) {
+            this.midichannel[5].noteOn(notes[i], 1000);
+        }
+    }
+}

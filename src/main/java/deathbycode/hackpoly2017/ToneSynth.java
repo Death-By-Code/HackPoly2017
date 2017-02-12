4ce3c8968bdc29d8b4db31823c72e63017bfa6a2
Joseph <jmcauthen@live.com>
2017-02-11 21:20:39 -0800

    Pushed the correct thing this time

--- src/main/java/deathbycode/hackpoly2017/ToneSynth.java
+++ src/main/java/deathbycode/hackpoly2017/ToneSynth.java
@@ -1,5 +1,3 @@
-package deathbycode.hackpoly2017;
-
 import javax.sound.midi.Instrument;
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
@@ -10,25 +8,41 @@ import javax.sound.midi.Synthesizer;
  * Created by Acer Customer on 2/11/2017.
  */
 public class ToneSynth {
-    Synthesizer syn;
-    MidiChannel[] midichannel;
-    Instrument[] instruments;
+    private Synthesizer syn;
+    private MidiChannel[] midichannel;
 
     public ToneSynth() {
         try {
             syn = MidiSystem.getSynthesizer();
             syn.open();
             midichannel = syn.getChannels();
+
+            for(int i = 0; i < syn.getAvailableInstruments().length; i++){
+                System.out.println(Integer.toString(i) + ": " + syn.getAvailableInstruments()[i].getName());
+            }
+
+
         } catch (MidiUnavailableException e) {
             e.printStackTrace();
         }
     }
 
 
-
-    public void play(int... notes) {
+    /*
+    Plays sequence all notes passed as arguments
+    @param notes: Piano keys in the range 0 - 127 with 60 as middle C
+    @param length: The duration in milliseconds that the notes will play
+     */
+    public void playNotes(int len, int... notes) {
         for(int i = 0; i < notes.length; i++) {
-            this.midichannel[5].noteOn(notes[i], 1000);
+            this.midichannel[2].noteOn(notes[i], len);
         }
+
+        try {
+            Thread.sleep(len);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.midichannel[2].allNotesOff();
     }
 }
